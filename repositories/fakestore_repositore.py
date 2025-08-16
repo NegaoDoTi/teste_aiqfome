22e85f49fa348e41f85e0ef998c3ee68f0c0ff8b
Vinicius Luciano da Silva <viniciusluciano2012@hotmai.com>
2025-08-16 01:07:14 -0300

    feat: criando o repositorio da fakestore

---
+++ repositories/fakestore_repositore.py
@@ -0,0 +1,17 @@
+from httpx import AsyncClient
+
+class FakeStoreRepositore:
+    def __init__(self):
+        self.products_url = "https://fakestoreapi.com/products/"
+    
+    
+    async def find_product(self, product_id:int) -> dict | None:
+        async with AsyncClient() as client_http:
+            response = client_http.get(url=f"{self.products_url}/{product_id}")
+            
+            if response.status_code == 200:
+                product = response.json()
+                
+                return product
+            
+            return None
