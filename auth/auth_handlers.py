245bbdb5c0f15a7ee853d5efd4ca6d8f6f618371
Vinicius Luciano da Silva <viniciusluciano2012@hotmai.com>
2025-08-16 01:05:59 -0300

    feat: criando funcao de jwt

--- auth/auth_handlers.py
+++ auth/auth_handlers.py
@@ -6,8 +6,21 @@ import jwt
 
 pwd_context = CryptContext(schemes=["bcrypt"])
 
-def verify_password(password:str, hash_password:str):
-    return pwd_context.verify(password, hash_password)
-
 def generate_password_hash(password:str) -> str:
     return pwd_context.hash(password)
+
+def generate_access_token(client_id:int, client_email:str, expires_time: timedelta = timedelta(hours=1)) -> str:
+    
+    now = datetime.now(ZoneInfo("America/Sao_Paulo"))
+    expire = now + expires_time
+    
+    payload = {
+        "client_id" : client_id,
+        "client_email" : client_email,
+        "iat" : now,
+        "exp" : expire
+    }
+    
+    encode_jwt = jwt.encode(payload, SECRECT_KEY, ALGORITHM)
+    
+    return encode_jwt
