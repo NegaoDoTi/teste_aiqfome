9f9800bcd3929004a2871273caac57a95aa753b8
Vinicius Luciano da Silva <viniciusluciano2012@hotmai.com>
2025-08-16 02:34:22 -0300

    feat: adicionando as novas rotas de login e favoritos

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from uvicorn import run
 from config.env import PORT, DEBUG
 from routes.clients import clients_route
+from routes.login import login_route
+from routes.favorites import favorites_route
 import logging
 
 logging.basicConfig(filename="server.log", level=logging.INFO, format="%(asctime)s [%(name)s] [%(levelname)s] - %(message)s",)
@@ -9,6 +11,8 @@ logging.basicConfig(filename="server.log", level=logging.INFO, format="%(asctime
 
 app = FastAPI(debug=DEBUG)
 app.include_router(clients_route)
+app.include_router(login_route)
+app.include_router(favorites_route)
 
 if __name__ == "__main__":
     run(app, host="0.0.0.0", port=PORT)
