48a0cdfd160da8b6d6a07809cd45c315ea241615
Vinicius Luciano da Silva <viniciusluciano2012@hotmai.com>
2025-08-16 01:09:07 -0300

    fix: corrindo algumas coisas na rota de clientes

--- routes/clients.py
+++ routes/clients.py
@@ -5,22 +5,22 @@ from views.clients_view import ClientsView
 
 clients_route = APIRouter()
 
-@clients_route.get("/clients/{client_id}", response_class=JSONResponse)
+@clients_route.get("/clientes/{client_id}", response_class=JSONResponse)
 async def find_client(client_id:int):
     return await ClientsView().find_clients(client_id)
 
-@clients_route.get("/clients", response_class=JSONResponse)
+@clients_route.get("/clientes", response_class=JSONResponse)
 async def find_clients():
     return await ClientsView().find_clients()
     
-@clients_route.post("/clients", response_class=JSONResponse)
+@clients_route.post("/clientes", response_class=JSONResponse)
 async def register_client(form: ClientRegisterForm = Depends(ClientRegisterForm.form)):
     return await ClientsView().register_client(form.name, form.email, form.password)
     
-@clients_route.delete("/clients/{client_id}", response_class=JSONResponse)
+@clients_route.delete("/clientes/{client_id}", response_class=JSONResponse)
 async def delete_client(client_id:int):
     return await ClientsView().delete_client(client_id)
 
-@clients_route.put("/clients/{client_id}")
+@clients_route.put("/clientes/{client_id}")
 async def update_client(client_id:int, client_data:ClientUpdate):
     return await ClientsView().update_client(client_id, name=client_data.name, email=client_data.email)
